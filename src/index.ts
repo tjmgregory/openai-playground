b110c4f4cead9f5b777765fe978a88cfdf4f63fd
Theo Gregory <tjmg@outlook.com>
2023-03-10 18:10:13 +0800

    feat: gets audio transcription working

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import "./config.ts";
 import { Configuration, OpenAIApi } from "openai";
+import { createReadStream } from "fs";
 const configuration = new Configuration({
   organization: process.env.OPENAI_ORGANISATION,
   apiKey: process.env.OPENAI_SECRET,
 });
 const openai = new OpenAIApi(configuration);
-const response = await openai.listEngines();
+
+const response = await openai.createTranscription(
+  createReadStream("assets/test_headphones_1.m4a") as any,
+  "whisper-1"
+);
 console.log(response.data);
